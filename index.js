3a9267314eef1ecb2908825c494247791b025fa0
Francois <asteryujano@gmail.com>
2019-10-01 14:43:52 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express')
+const mongoose = require('mongoose')
+require('dotenv/config')
+const Product = require('./models/Product')
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+mongoose.connect(
+	process.env.DB_CONNECTION, 
+	{
+		useNewUrlParser : true,
+		useUnifiedTopology: true
+	}
+);
+
+//routes
+const resultsLimit = new Number(process.env.RESULTS_LIMIT) || 25;
+
+app.get('/', async (req, res) => {
+	try{
+		const products = await Product.find().limit(resultsLimit);
+		res.json(products)
+	}catch(err){
+		res.json({message: err})
+	}
+})
+
+//TO DELETE
+app.get('/feed', async (req, res) => {
+	const product = new Product({
+		title: "New title!"
+	})
+	try {
+		const savedProduct = await product.save();
+		res.json(savedProduct)
+	}catch(err){
+		console.log(err);
+	}
+})
+
+
+app.listen(
+	process.env.PORT, 
+	() => console.log(`\n[+] Listening on ${process.env.PORT}...\n`)
+)
